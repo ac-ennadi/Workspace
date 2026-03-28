9a05422619ddfc00c847cc6906523f9623168de8
ac-ennadi <achrafennadiri@gmail.com>
2026-03-28 13:14:00 +0100

    namespaces

--- oop/main.cpp
+++ oop/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 
+//the namespace
+
+///NOTE : we can use the name space is room
+// we can solve the problem of name conflex
+
+// void endl(void) {std::cout << std::endl;}
+
+// namespace room1 {
+//     void someone ()
+//     {
+//         std::cout << "i am here";
+//     }
+// };
+
+// namespace room2 {
+//     void someone ()
+//     {
+//         std::cout << "i am here";
+//     }
+// };
 
 int main()
 {
-    std::cout << "Done";
-    return 0;
+	
+	return (0);
 }
