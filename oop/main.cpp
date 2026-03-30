8d95bd2c68bcedd429728e347780422b66bc2619
ac-ennadi <achrafennadiri@gmail.com>
2026-03-30 11:33:43 +0100

    push

--- oop/main.cpp
+++ oop/main.cpp
@@ -1,11 +1,25 @@
-#include "tools.hpp"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acennadi <acennadi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/30 11:29:05 by acennadi          #+#    #+#             */
+/*   Updated: 2026/03/30 11:33:25 by acennadi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "things.hpp"
 
 void endl(void) {std::cout << std::endl;}
 
 int main()
 {
-	room1::someone();
-	endl();
-	room2::someone();
+	//this was for the namespace 
+	// room1::someone();
+	// endl();
+	// room2::someone();
+	
 	return (0);
 }
--- oop/things.hpp
+++ oop/things.hpp
@@ -1,14 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   classes.cpp                                        :+:      :+:    :+:   */
+/*   things.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acennadi <acennadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/30 11:29:00 by acennadi          #+#    #+#             */
-/*   Updated: 2026/03/30 11:29:42 by acennadi         ###   ########.fr       */
+/*   Created: 2026/03/30 11:29:11 by acennadi          #+#    #+#             */
+/*   Updated: 2026/03/30 11:29:12 by acennadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "things.hpp"
+#include <iostream>
 
+//the namespace
+
+///NOTE : we can use the name space is room
+// we can solve the problem of name conflex
+
+namespace room1 {
+    void someone (void) {std::cout << "i am here";}
+};
+
+namespace room2 {
+    void someone (void) {std::cout << "i am here";}
+};
