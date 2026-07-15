4de97b5ee1ba0b96bd49da1776c02efd34f41c91
0ac_raf0dev <achrafennadiri@gmail.com>
2026-07-15 19:31:49 +0100

    inheritance

--- c++/Doc00/main.cpp
+++ c++/Doc00/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iterator>
 #include <string>
 
 // Encapsulation
@@ -19,13 +20,19 @@ class Person
         std::string sname;
     public: //->> this is the public keyword
     //methods -->> functions
-        Person() {} //this is the default constractor
+        Person() = default; // U can use 'Person() {}' this is the default constractor
         Person(std::string fname, std::string sname): fname(fname), sname(sname){} //this is the custom  constractor
         void PrintFullName();
         std::string GetfullName(); //getter
         void SetFullName(std::string fname, std::string sname); //  Setters
 };
 
+
+void Person::SetFullName(std::string fname, std::string sname) {(*this).fname = fname; this->sname = sname;}
+
+void Person::PrintFullName()    {std::cout << fname << " " << sname << std::endl;}
+
+std::string Person::GetfullName()   {return fname + "" + sname;}
 //inheritance
 
 //child class (derived class) 
@@ -35,24 +42,17 @@ class Someone : public Person
         std::string Departement;
     public:
         Someone (std::string FirstName, std::string LastName, std::string Departement) : Person(FirstName, LastName), Departement(Departement){}
-    
+        void    getInfo();
 };
 
-void Person::SetFullName(std::string fname, std::string sname) {(*this).fname = fname; this->sname = sname;}
-
-void Person::PrintFullName()    {std::cout << fname << " " << sname << std::endl;}
-
-std::string Person::GetfullName()   {return fname + "" + sname;}
+void Someone::getInfo() {std::cout << "Full Name: " << GetfullName() << std::endl << "Departement: " << Departement << std::endl;};
 
 int main()
 {
     // instances == object -> Creating instances of the class 'person'
     // Interacting == create an object from a class 
-    Someone person;
-
+    Someone person("achraf", "ennadiri", "1337");
+    person.getInfo();
     // Interacting -> Interacting with the instances
-    person.PrintFullName();
-    person.SetFullName("hamza", "kadir");
-    person.PrintFullName();
     return (0);
 }
