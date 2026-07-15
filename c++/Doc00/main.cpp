0d504c9f7a3d76552f534025bd538728905b04ea
0ac_raf0dev <achrafennadiri@gmail.com>
2026-07-15 19:21:28 +0100

    push

--- c++/Doc00/main.cpp
+++ c++/Doc00/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <string>
 
+// Encapsulation
+
 // private: Members can only be accessed from inside the class itself. 
 //             External code cannot see or change them.
 // public: Members can be accessed from anywhere outside the class.
 
 // class -> Blue Print
+
+//parent class (base class) --> inherited from
 class Person
 {
     //data members -->> varibles
@@ -14,12 +18,24 @@ class Person
         std::string fname;
         std::string sname;
     public: //->> this is the public keyword
-        //methods -->> functions
-        Person() = default; //this is the default constractor
+    //methods -->> functions
+        Person() {} //this is the default constractor
         Person(std::string fname, std::string sname): fname(fname), sname(sname){} //this is the custom  constractor
         void PrintFullName();
         std::string GetfullName(); //getter
         void SetFullName(std::string fname, std::string sname); //  Setters
+};
+
+//inheritance
+
+//child class (derived class) 
+class Someone : public Person
+{
+    private:
+        std::string Departement;
+    public:
+        Someone (std::string FirstName, std::string LastName, std::string Departement) : Person(FirstName, LastName), Departement(Departement){}
+    
 };
 
 void Person::SetFullName(std::string fname, std::string sname) {(*this).fname = fname; this->sname = sname;}
@@ -32,8 +48,7 @@ int main()
 {
     // instances == object -> Creating instances of the class 'person'
     // Interacting == create an object from a class 
-    Person person("Achraf", "ennadiri");
-    Person person1;
+    Someone person;
 
     // Interacting -> Interacting with the instances
     person.PrintFullName();
