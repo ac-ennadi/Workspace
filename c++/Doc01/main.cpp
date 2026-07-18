5105863aa84967ee79f8c1b025a25203bb330756
ac-ennadi <achrafennadiri@gmail.com>
2026-07-18 21:17:11 +0100

    Polymorphism

--- c++/Doc01/main.cpp
+++ c++/Doc01/main.cpp
@@ -1,7 +1,35 @@
 # include <iostream>
 
+class Animals {
+    public:
+        virtual void animalsound();
+};
+
+void Animals::animalsound() {std::cout << "animal make a sound!\n";}
+
+class Dog : public Animals {
+    public:
+        void animalsound() override;
+};
+
+void Dog::animalsound() {std::cout << "dog make a sound!\n";}
+
+class Cat : public Animals {
+    public:
+        void animalsound() override;
+};
+
+void Cat::animalsound() {std::cout << "cat make a sound!\n";}
+
+
 int main()
 {
+    Animals animal;
+    Dog dog;
+    Cat cat;
     
+    animal.animalsound();
+    dog.animalsound();
+    cat.animalsound();
     return 0;
 }
