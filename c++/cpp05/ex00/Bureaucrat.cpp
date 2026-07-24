8c299dca8984d27c8e7413b823210f1bc113a63a
ac-ennadi <achrafennadiri@gmail.com>
2026-07-24 16:47:29 +0100

    update

---
+++ c++/cpp05/ex00/Bureaucrat.cpp
@@ -0,0 +1,9 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() {}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &other) {}
+
+Bureaucrat Bureaucrat::operator+(const Bureaucrat &other) {}
+
+Bureaucrat::~Bureaucrat() {}
--- c++/cpp05/ex00/Bureaucrat.hpp
+++ c++/cpp05/ex00/Bureaucrat.hpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <string>
 
 class Bureaucrat {
     private:
         const std::string name;
-        int grade[150];
+        int grade;
     public:
         Bureaucrat();
+        Bureaucrat(const Bureaucrat &other);
+        Bureaucrat operator+(const Bureaucrat &other);
         ~Bureaucrat();
 };
