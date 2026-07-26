b40d28e01d9cf6f8a1439b21e078161b38bb228c
ac-ennadi <achrafennadiri@gmail.com>
2026-07-26 21:48:48 +0100

    exception

--- c++/cpp05/ex00/main.cpp
+++ c++/cpp05/ex00/main.cpp
@@ -1,7 +1,24 @@
+# include <iostream>
+#include <stdexcept>
 # include "Bureaucrat.hpp"
+
+using namespace std;
 
 int main()
 {
-    
+    string str = "achraf";
+
+    try
+    {
+        std::cout << str.at(9) << endl;
+    }
+    catch (out_of_range &e)
+    {
+        cout << "excption: " << e.what() << endl;
+    }
+    catch (...)
+    {
+        cout << "Default excption\n";
+    }
     return (0);
 }
