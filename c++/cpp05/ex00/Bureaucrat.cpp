46921e0914a3f398c3f613252aca6f5d86bf48f8
ac-ennadi <achrafennadiri@gmail.com>
2026-07-26 18:44:27 +0100

    ex00::cpp05

--- c++/cpp05/ex00/Bureaucrat.cpp
+++ c++/cpp05/ex00/Bureaucrat.cpp
@@ -1,9 +1,15 @@
 #include "Bureaucrat.hpp"
 
+
 Bureaucrat::Bureaucrat() {}
-
+Bureaucrat::Bureaucrat(const int Grade, const std::string Name)
+: Name(Name), Grade(Grade) {}
 Bureaucrat::Bureaucrat(const Bureaucrat &other) {}
-
-Bureaucrat Bureaucrat::operator+(const Bureaucrat &other) {return Bureaucrat(*this);}
-
+Bureaucrat Bureaucrat::operator<<(const Bureaucrat &other) {return Bureaucrat(*this);}
 Bureaucrat::~Bureaucrat() {}
+
+int Bureaucrat::getGrade() {return (this->Grade);}
+std::string Bureaucrat::getName() {return (this->Name);}
+
+void Bureaucrat::incrementGrade() {(this->Grade++);}
+void Bureaucrat::decrementGrade() {(this->Grade--);}
--- c++/cpp05/ex00/Bureaucrat.hpp
+++ c++/cpp05/ex00/Bureaucrat.hpp
@@ -2,11 +2,21 @@
 
 class Bureaucrat {
     private:
-        const std::string name;
-        int grade;
+        const std::string Name;
+        int Grade;
     public:
         Bureaucrat();
+        Bureaucrat(const int Grade, const std::string Name);
         Bureaucrat(const Bureaucrat &other);
-        Bureaucrat operator+(const Bureaucrat &other);
+        Bureaucrat operator<<(const Bureaucrat &other);
         ~Bureaucrat();
+
+        int getGrade();
+        std::string getName();
+        void incrementGrade();
+        void decrementGrade();
+
+        void GradeTooHighException();
+        void GradeTooLowException();
+
 };
